23a9f517b6bffa401a512fba3c5dc4591a742bc5
Diesther <diestherlara8@gmail.com>
2026-05-20 09:49:30 -0600

    Remove median age calculation and null value filling from Titanic dataset analysis

--- Titanic/titanic.py
+++ Titanic/titanic.py
@@ -50,10 +50,3 @@ print(f"Mediana calculada para la edad: {mediana_edad}")
 df['edad'] = df['edad'].fillna(mediana_edad)
 
 df_resultado = df.drop(columns=['pasajero_id'])
-
-print("-" * 40)
-print("Conteo de nulos finalizado:")
-print(df_resultado.isnull().sum())
-
-print("\nPrimeros 5 registros del DataFrame limpio:")
-print(df_resultado.head())
